77f823ac03a34d7bdcbe0bf20753fd67103b08f1
JackalRegalia <antexsol93@gmail.com>
2018-06-14 21:09:28 +0530

    Initial Commit

---
+++ Client/Client/Source.cpp
@@ -0,0 +1,328 @@
+#include <vector>
+#include <iostream>
+#include <set>
+#include <cassert>
+
+#include "json.hpp"
+
+#include <Windows.h>
+
+using nlohmann::json;
+
+
+namespace {
+	::std::vector<uint8_t> buffer;
+
+	json build_function_string(const json& function) {
+		json command;
+		command["request"] = "function";
+		command["call"] = function["name"];
+		if (function.find("args") != ::std::end(function)) {
+			for (auto& arg : function["args"]) {
+				::std::cout << "Enter value for " << arg["name"].get<::std::string>() <<"\n";
+				if (arg["type"] == "float") {
+					float in;
+					::std::cin >> in;
+					::std::string arg_name = arg["name"];
+					command[arg_name] = in;
+				}
+				else if (arg["type"] == "int") {
+					int in;
+					::std::cin >> in;
+					::std::string arg_name = arg["name"];
+					command[arg_name] = in;
+				}
+				else if (arg["type"] == "double") {
+					double in;
+					::std::cin >> in;
+					::std::string arg_name = arg["name"];
+					command[arg_name] = in;
+				}
+			}
+		}
+
+		return command;
+	}
+
+	struct ClassInfo {
+		size_t size;
+		::std::string name;
+		json functions;
+
+		friend bool operator<(const ClassInfo& c1, const ClassInfo& c2) {
+			return c1.name < c2.name;
+		}
+	};
+
+	struct ClientObject {
+		::std::string object_name;
+		::std::vector<uint8_t> data;
+	};
+
+	void print(uint8_t* buffer, const ::std::string& type) {
+		if (type == "float") {
+			::std::cout << *reinterpret_cast<float*>(buffer);
+		}
+		else if (type == "double") {
+			::std::cout << *reinterpret_cast<double*>(buffer);
+		}
+		else if (type == "int") {
+			::std::cout << *reinterpret_cast<int*>(buffer);
+		}
+		::std::cout << "\n";
+	}
+
+	class PipeConnection {
+		::std::set<ClassInfo> types;
+
+		::std::map<ClassInfo, ::std::vector<ClientObject>> objects;
+
+		::std::tuple<bool, OVERLAPPED, ::std::string> overlapped_info;
+	public:
+		const HANDLE pipe;
+
+		PipeConnection(const char* name) : pipe(CreateFile(TEXT("\\\\.\\pipe\\Pipe"),
+			GENERIC_READ | GENERIC_WRITE,
+			FILE_SHARE_READ | FILE_SHARE_WRITE,
+			NULL,
+			OPEN_EXISTING,
+			FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,
+			NULL)){
+			if (pipe == INVALID_HANDLE_VALUE) {
+				throw ::std::exception("Pipe Connection could not be established");
+			}
+
+			json command;
+			command["request"] = "meta";
+			
+			auto json_bytes = nlohmann::json::to_ubjson(command);
+
+			if (!WriteFile(pipe, json_bytes.data(), sizeof(uint8_t) * json_bytes.size(), NULL, NULL)) {
+				throw ::std::exception("Meta data could not be fetched.");
+			}
+
+			DWORD bytes_in_pipe;
+			buffer.resize(1024);
+			if (!ReadFile(pipe, buffer.data(), 1024, &bytes_in_pipe, NULL)) {
+				throw ::std::exception("Meta data could not be fetched.");
+			}
+
+			buffer.resize(bytes_in_pipe);
+			json meta = nlohmann::json::from_ubjson(buffer);
+
+			ClassInfo info;
+			info.name = meta["name"].get<::std::string>();
+			info.functions = meta["functions"];
+			info.size = meta["size"];
+
+			types.insert(info);
+
+			::std::get<0>(overlapped_info) = true;
+		}
+
+		bool is_open() const {
+			return pipe != INVALID_HANDLE_VALUE;
+		}
+
+		bool run() {
+			buffer.resize(1024);
+
+			if (!::std::get<0>(overlapped_info)) {
+				if (HasOverlappedIoCompleted(&::std::get<1>(overlapped_info))) {
+					DWORD bytes_read;
+					ReadFile(pipe, buffer.data(), 1024, NULL, &::std::get<1>(overlapped_info));
+					::std::cout << "Async function result is\n";
+					print(buffer.data(), ::std::get<2>(overlapped_info));
+					::std::get<0>(overlapped_info) = true;
+				}
+			}
+
+			::std::cout << 
+				"1. Get available types\n" <<
+				"2. Get existing objects\n" <<
+				"3. Create new object" << "\n" <<
+				"4. Operate on existing object" << "\n" <<
+				"5. Close Connection\n";
+			int choice;
+			::std::cin >> choice;
+			switch (choice)
+			{
+			case 1: {
+				::std::cout << "Available types are :\n";
+				for (auto& type : types) {
+					::std::cout << type.name << "\n";
+				}
+				return true;
+			}
+			case 2: {
+				::std::cout << "Available objects are\n";
+
+				for (auto& pair : objects) {
+					for (auto& obj : pair.second) {
+						::std::cout << obj.object_name << "\n";
+					}
+				}
+				
+				return true;
+			}
+			case 3: {
+				if (!::std::get<0>(overlapped_info)) {
+					::std::cout << "Async operation in progress. Pipe inaccessible!\n";
+					return true;
+				}
+
+				::std::cout << "Available types are :\n";
+				for (auto& type : types) {
+					::std::cout << type.name << "\n";
+				}
+
+				::std::string requested_type;
+				::std::cin >> requested_type;
+
+				ClassInfo key;
+				key.name = requested_type;
+				const auto& type = types.find(key);
+
+				::std::cout << "Enter name of new object\n";
+				::std::string obj_name;
+				::std::cin >> obj_name;
+
+				json command;
+				command["request"] = "creation";
+				command["type"] = requested_type;
+				auto request_bytes = nlohmann::json::to_ubjson(command);
+
+				if (WriteFile(pipe, request_bytes.data(), request_bytes.size(), NULL, NULL)) {
+
+					ClientObject obj;
+					obj.data.resize(type->size);
+					obj.object_name = obj_name;
+
+					if (ReadFile(pipe, obj.data.data(), type->size, NULL, NULL)) {
+						objects[key].push_back(::std::move(obj));
+						::std::cout << "Object created\n";
+					}
+				}
+				return true;
+			}
+
+			case 4: {
+				if (!::std::get<0>(overlapped_info)) {
+					::std::cout << "Async operation in progress. Pipe inaccessible!\n";
+					return true;
+				}
+
+				::std::cout << "Types of object to be operated on\n";
+				for (auto& type : types) {
+					::std::cout << type.name << "\n";
+				}
+
+				::std::cout << "Enter type\n";
+				::std::string requested_type;
+				::std::cin >> requested_type;
+
+
+				ClassInfo key;
+				key.name = requested_type;
+				if (objects[key].empty()) {
+					return true;
+				}
+
+				::std::cout << "Available objects are\n";
+
+				for (auto& obj : objects[key]) {
+					::std::cout << obj.object_name << "\n";
+				}
+
+				::std::cout << "Enter object name to operate on\n";
+				::std::string object_name;
+				::std::cin >> object_name;
+
+				auto it_obj = ::std::find_if(::std::begin(objects[key]), ::std::end(objects[key]),
+					[object_name](const ClientObject& obj) {return obj.object_name == object_name; });
+
+				if (it_obj == ::std::end(objects[key])) {
+					return true;
+				}
+
+				::std::cout << "Available functions are\n";
+				auto it_set = types.find(key);
+				for (auto& fun : it_set->functions) {
+					::std::cout << fun["name"].get<::std::string>() <<"\n";
+				}
+
+				::std::cout << "Enter function name\n";
+				::std::string function_name;
+				::std::cin >> function_name;
+
+				auto it_fun = ::std::find_if(::std::begin(it_set->functions), ::std::end(it_set->functions),
+					[function_name](const json& j) {return j["name"] == function_name; });
+				json command = build_function_string(*it_fun);
+				command["type"] = requested_type;
+				command["orig_data"] = it_obj->data;
+
+				auto json_bytes = json::to_ubjson(command);
+				if ((*it_fun)["fun_type"] == "async") {
+					assert(it_fun->find("out") != ::std::end(*it_fun));
+					::std::get<0>(overlapped_info) = false;
+					::std::get<1>(overlapped_info) = {};
+					::std::get<2>(overlapped_info) = (*it_fun)["out"].get<::std::string>();
+					if (!WriteFile(pipe, json_bytes.data(), json_bytes.size(), NULL, NULL)) {
+						// Reset and return;
+						::std::get<0>(overlapped_info) = true;
+						return true;
+					}
+					DWORD bytes_read;
+					if (ReadFile(pipe, buffer.data(), 1024, &bytes_read, &::std::get<1>(overlapped_info))) {
+						::std::cout << "Async function result is\n";
+						print(buffer.data(), ::std::get<2>(overlapped_info));
+						::std::get<0>(overlapped_info) = true;
+					}
+				}
+				else if(WriteFile(pipe, json_bytes.data(), json_bytes.size(), NULL, NULL)) {
+					DWORD bytes_in_pipe;
+					if (ReadFile(pipe, buffer.data(), 1024, &bytes_in_pipe, NULL)) {
+						buffer.resize(bytes_in_pipe);
+						if (it_fun->find("out") != ::std::end(*it_fun)) {
+							print(buffer.data(), (*it_fun)["out"]);
+						}
+						else {
+							it_obj->data = buffer;
+							::std::cout << "Object moved.\n";
+						}
+					}
+				}
+
+				return true;;
+			}
+			case 5: {
+				if (!::std::get<0>(overlapped_info)) {
+					::std::cout << "Async operation in progress. Cannot close!\n";
+					return true;
+				}
+				json command;
+				command["request"] = "close";
+
+				auto json_bytes = json::to_ubjson(command);
+				WriteFile(pipe, json_bytes.data(), json_bytes.size(), NULL, NULL);
+				return false;
+			}
+			default:
+				return true;
+			}
+		}
+
+		~PipeConnection() {
+			CloseHandle(pipe);
+		}
+	};
+}
+
+int main(void)
+{
+	PipeConnection pconn("\\\\.\\pipe\\Pipe");
+	while (pconn.run()){
+	}
+
+	return (0);
+}
---
+++ Server/Server/Source.cpp
@@ -0,0 +1,149 @@
+#include <Windows.h>
+
+#include <type_traits>
+
+#include "json.hpp"
+
+using nlohmann::json;
+
+namespace {
+
+	struct point {
+		float x;
+		float y;
+
+		float get_x() const {
+			return x;
+		}
+
+		float get_y() const {
+			return y;
+		}
+
+		void move(float by_x, float by_y) {
+			x += by_x;
+			y += by_y;
+		}
+
+		float slow_add() {
+			Sleep(2000);
+			float sum = 0.0f;
+			sum += x;
+			sum += y;
+			return sum;
+		}
+
+		static void service_request(const HANDLE pipe, const json& request) {
+			assert(pipe != INVALID_HANDLE_VALUE);
+			if (request["call"] == "get_x") {
+				::std::vector<uint8_t> data = request["orig_data"];
+				point point_ = *reinterpret_cast<point*>(data.data());
+				float x = point_.get_x();
+				WriteFile(pipe, &x, sizeof(float), NULL, NULL);
+			}
+			else if (request["call"] == "get_y") {
+				::std::vector<uint8_t> data = request["orig_data"];
+				point point_ = *reinterpret_cast<point*>(data.data());
+				float y = point_.get_y();
+				WriteFile(pipe, &y, sizeof(float), NULL, NULL);
+			}
+			else if (request["call"] == "move") {
+				::std::vector<uint8_t> data = request["orig_data"];
+				point point_ = *reinterpret_cast<point*>(data.data());
+				float x = request["x"];
+				float y = request["y"];
+				point_.move(x, y);
+				WriteFile(pipe, &point_, sizeof(point), NULL, NULL);
+			}
+			else if (request["call"] == "slow_add") {
+				::std::vector<uint8_t> data = request["orig_data"];
+				point point_ = *reinterpret_cast<point*>(data.data());
+				float sum = point_.slow_add();
+				WriteFile(pipe, &sum, sizeof(float), NULL, NULL);
+			}
+		}
+
+	};
+
+
+	template <typename T>
+	json get_meta() {}
+
+	template <> json get_meta<point>() {
+		json meta;
+		meta["name"] = "point";
+		meta["size"] = sizeof(point);
+		meta["functions"].push_back({ { "name", "get_x" },{ "out", "float" } , {"fun_type", "sync"} });
+		meta["functions"].push_back({ { "name", "get_y" },{ "out", "float" } ,{ "fun_type", "sync" } });
+		meta["functions"].push_back({ { "name", "move" },{ "args",{ { { "name", "x" },{ "type", "float" } },
+			{ { "name", "y" },{ "type", "float" } } } }, { "fun_type", "sync" } });
+		meta["functions"].push_back({ {"name", "slow_add"}, { "out", "float" }, { "fun_type", "async" } });
+		return meta;
+	}
+
+	::std::vector<uint8_t> buffer;
+
+	class NamedPipe {
+	public:
+		const HANDLE pipe;
+
+		NamedPipe(const char* name) : pipe(CreateNamedPipe("\\\\.\\pipe\\Pipe", 
+			PIPE_ACCESS_DUPLEX | 
+			FILE_FLAG_OVERLAPPED, 
+			PIPE_TYPE_BYTE | PIPE_READMODE_BYTE, 
+			1, 1024, 1024, 0, NULL)) {
+			if (pipe == INVALID_HANDLE_VALUE) {
+				throw ::std::exception("Pipe could not be created");
+			}
+		}
+
+		void handle_connection() {
+			if (ConnectNamedPipe(pipe, NULL) != FALSE) {
+				DWORD bytes_read;
+				buffer.resize(1024);
+				while (ReadFile(pipe, buffer.data(), 1024, &bytes_read, NULL) != FALSE){
+					buffer.resize(bytes_read);
+					json command = nlohmann::json::from_ubjson(buffer);
+					if (command["request"] == "meta") {
+						json meta = get_meta<point>();
+						auto data = json::to_ubjson(meta);
+						WriteFile(pipe, data.data(), data.size(), NULL, NULL);
+					}
+
+					if (command["request"] == "creation") {
+						if (command["type"] == "point") {
+							point new_point{ 0.0f, 0.0f };
+							WriteFile(pipe, &new_point, sizeof(point), NULL, NULL);
+						}
+					}
+
+					if (command["request"] == "function") {
+						if (command["type"] == "point") {
+							point::service_request(pipe, command);
+						}
+					}
+
+					if (command["request"] == "close") {
+						DisconnectNamedPipe(pipe);
+						break;
+					}
+
+
+					buffer.resize(1024);
+				}
+			}
+		}
+
+	};
+}
+
+int main() {
+	
+
+	NamedPipe npipe("\\\\.\\pipe\\Pipe");
+
+	while (true) {
+		npipe.handle_connection();
+	}
+	return 0;
+}
